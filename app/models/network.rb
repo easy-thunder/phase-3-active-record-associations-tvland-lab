d785b627abbcd01469ba4375605dfaf346c82ff5
Jacob David Diehl <jakediehl17@gmail.com>
2023-02-05 12:10:32 -0700

    commit tree e0667add40eca8dc834fafaf5283b2381a535069

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,3 +1,10 @@
 class Network < ActiveRecord::Base
+    has_many :shows
+    has_many :actors through: :shows
+
+    def sorry
+        
+        "We're sorry about passing on #{self.actor.find_by(name: )}'s pilot"
+    end
   
 end
